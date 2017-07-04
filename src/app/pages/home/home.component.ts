e5a7fee130420421ac191b3bcb539e396f5d763f
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-07-04 11:59:07 +0300

    [feat]: use Title service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  pageTitle = 'MeetupMgr | Home';
 
-  constructor() { }
+  constructor(private title: Title) { }
 
   ngOnInit() {
+    this.title.setTitle(this.pageTitle);
   }
 
 }
