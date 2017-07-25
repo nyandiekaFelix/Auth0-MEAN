cf93863fbd3d559954adc8811542549ce9171233
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-07-25 17:28:33 +0300

    [feat]: add callback and events routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { EventsComponent } from './pages/events/events.component';
+import { CallbackComponent } from './pages/callback/callback.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  }
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'callback',
+        component: CallbackComponent
+    },
+    {
+        path: 'events',
+        component: EventsComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
